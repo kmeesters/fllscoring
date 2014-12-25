59d05e858c44795f30ffdccc779760668274dded
rikkertkoppes <rikkert@rikkertkoppes.com>
2014-12-25 16:49:51 +0100

    completed tests for ng-fs

--- spec/services/ng-fsSpec.js
+++ spec/services/ng-fsSpec.js
@@ -6,30 +6,40 @@ describe('ng-fs',function() {
         'services/fs': fsMock
     });
 
-    var $fs
+    var $fs;
 
     beforeEach(function() {
         angular.mock.module(module.name);
         angular.mock.inject(['$fs',function(fs) {
             $fs = fs;
-        }])
+        }]);
     });
 
     describe('reading',function() {
-        it('should return the parsed read data',function(done) {
-            $fs.read('foo').then(function(res) {
+        it('should return the parsed read data',function() {
+            return $fs.read('foo').then(function(res) {
                 expect(res).toEqual('dummydata');
-                done();
+            });
+        });
+        it('should reject if something fails',function() {
+            fsMock.read.andReturn(Q.reject('squeek'));
+            return $fs.read('foo').catch(function(err) {
+                expect(err).toEqual('squeek');
             });
         });
     });
 
     describe('writing',function() {
-        it('should call the write spy and append a line end',function(done) {
-            $fs.write('filename','lalal').then(function() {
+        it('should call the write spy and append a line end',function() {
+            return $fs.write('filename','lalal').then(function() {
                 expect(fsMock.write).toHaveBeenCalledWith('filename','"lalal"\n');
-                done();
             });
-        })
+        });
+        it('should reject if something fails',function() {
+            fsMock.write.andReturn(Q.reject('squeek'));
+            return $fs.write('filename','lalal').catch(function(err) {
+                expect(err).toEqual('squeek');
+            });
+        });
     });
 });
